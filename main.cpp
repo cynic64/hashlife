5a4f4e0274c7fd4d8119332ae5df13a50c403c2c
Nicholas Schweitzer <f3bl@gmx.com>
2020-11-05 08:08:18 +0100

    Randomization works

--- main.cpp
+++ main.cpp
@@ -1,21 +1,28 @@
 #include<iostream>
 #include<vector>
+#include<cstdlib>
 
 class World {
 	std::vector<std::vector<bool>> grid;
+	std::vector<std::vector<bool>> next;
 public:
 	World(int width, int height);
 	void print();
+	void randomize();
 };
 
 int main() {
 	World world(8, 8);
 
 	world.print();
+	std::cout << "Randomizing..." << std::endl;
+	world.randomize();
+	world.print();
 }
 
 World::World(int width, int height) {
-	grid = std::vector<std::vector<bool>> (height, std::vector<bool>(width));
+	grid = std::vector<std::vector<bool>>
+		(height, std::vector<bool>(width, false));
 }
 
 void World::print() {
@@ -25,4 +32,13 @@ void World::print() {
 		}
 		std::cout << std::endl;
 	}
+}
+
+void World::randomize() {
+	for (auto row = grid.begin(); row != grid.end(); ++row) {
+		for (auto cell = row->begin(); cell != row->end(); ++cell) {
+			*cell = rand() % 2;
+		}
+	}
+
 }
