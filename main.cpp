5d096efc64e865292330a6f44e5f0dc3942e08e6
Nicholas Schweitzer <f3bl@gmx.com>
2020-11-05 08:18:16 +0100

    Temp commit

--- main.cpp
+++ main.cpp
@@ -1,17 +1,32 @@
 #include<iostream>
 #include<vector>
+#include<array>
 #include<cstdlib>
 
-class World {
+const std::array<std::array<int,2>, 8> NEIGHBOR_OFFSETS {{
+	{{-1,-1}},
+	{{0,-1}},
+	{{1,-1}},
+	{{-1,0}},
+	{{1,0}},
+	{{-1,1}},
+	{{0,1}},
+	{{1,1}}
+	}};
+
+class World
+{
 	std::vector<std::vector<bool>> grid;
 	std::vector<std::vector<bool>> next;
 public:
 	World(int width, int height);
 	void print();
 	void randomize();
+	int count_neighbors(int x, int y);
 };
 
-int main() {
+int main()
+{
 	World world(8, 8);
 
 	world.print();
@@ -20,12 +35,14 @@ int main() {
 	world.print();
 }
 
-World::World(int width, int height) {
+World::World(int width, int height)
+{
 	grid = std::vector<std::vector<bool>>
 		(height, std::vector<bool>(width, false));
 }
 
-void World::print() {
+void World::print()
+{
 	for (auto row : grid) {
 		for (auto cell : row) {
 			std::cout << (cell ? "# " : ". ");
@@ -34,11 +51,17 @@ void World::print() {
 	}
 }
 
-void World::randomize() {
+void World::randomize()
+{
 	for (auto row = grid.begin(); row != grid.end(); ++row) {
 		for (auto cell = row->begin(); cell != row->end(); ++cell) {
 			*cell = rand() % 2;
 		}
 	}
 
+}
+
+int World::count_neighbors(int x, int y)
+{
+	
 }
